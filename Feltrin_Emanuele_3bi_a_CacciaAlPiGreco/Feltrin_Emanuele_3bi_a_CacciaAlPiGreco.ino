028f52e50ad6dab00e3082c11e837744cb4b4ea3
FeltrinEmanuele <emanuelefeltrin19971010@gmail.com>
2019-05-17 18:23:20 +0200

    Buon Punto

--- Feltrin_Emanuele_3bi_a_CacciaAlPiGreco/Feltrin_Emanuele_3bi_a_CacciaAlPiGreco.ino
+++ Feltrin_Emanuele_3bi_a_CacciaAlPiGreco/Feltrin_Emanuele_3bi_a_CacciaAlPiGreco.ino
@@ -2,6 +2,7 @@
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 int NumeroRandom;
+int PosizioneRandom;
 int Bottone1;
 int Bottone2;
 int Bottone3;
@@ -14,16 +15,12 @@ int Punti_PartitaCorrente;
 int PosizionePiGreco;
 int PosizionePenality;
 int PosizioneJolly;
-double Tempo;
-int ArrayPiGreco[5];
+int ArrayBottoni[5] = { Bottone1, Bottone2, Bottone3, Bottone4, Bottone5};
+int ArrayPosizione[5] = {0, 4, 8, 12, 15};
 
 void setup() {
   lcd.begin(16, 2);
   Record = 0;
-  Vite = 3;
-  Punti_PartitaCorrente = 0;
-  Tempo = 1.5;
-  int ArrayBottoni[5] = { Bottone1, Bottone2, Bottone3, Bottone4, Bottone5};
   Bottone1 = 2;
   Bottone2 = 3;
   Bottone3 = 4;
@@ -38,7 +35,6 @@ void setup() {
   pinMode (Bottone5,INPUT);
   pinMode (BottoneIniziale,INPUT);
 
-  SchermataIniziale();
 }
 
 void SchermataIniziale()
@@ -47,24 +43,115 @@ void SchermataIniziale()
   lcd.setCursor(0,0);
   lcd.print("Start Game");
   lcd.setCursor(0,1);
-  lcd.print("Record:" + String(Record));
+  lcd.print("Il Record: " + String(Record));
 }
 
-void AggiornaRecord(int record, int Punti_PartitaCorrente)
+void SchermataGioco()
 {
-  if(Punti_PartitaCorrente>record)
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Vite: " +String(Vite));
+  lcd.setCursor(9,0);
+  lcd.print("Punti: " + String(Punti_PartitaCorrente));
+}
+
+
+void AggiornaRecord(int record, int Punti)
+{
+  if(Punti>record)
   {
-    Record = Punti_PartitaCorrente;
+    Record = Punti;
   }
 }
 
-void NumeroRandomMetodo()
+void NumeroRandomMetodo(int valoreDa, int valoreA)
 {
-  NumeroRandom = random(0,4);
+  NumeroRandom = random(valoreDa,valoreA);
+}
+
+void BottoneInizioMetodo( int bottone)
+{
+  bool fatto = false;
+  while(!fatto)
+  {
+    if(digitalRead(bottone) == HIGH)
+    {
+      fatto=true;
+    }
+  }
+}
+
+void Gioco()
+{
+  bool fatto = false;
+  SchermataGioco();
+  while(!fatto)
+  {
+    NumeroRandomMetodo(0,9);
+    if(NumeroRandom >=0 && NumeroRandom <=5)
+    {
+      PosizioneRandom = random(0,4);
+      lcd.setCursor(ArrayPosizione[PosizioneRandom],1);
+      lcd.print("π");
+      int Tempo1 = millis();
+      int Tempo2;
+      bool finito = false;
+      while(!finito)
+      {
+        if(digitalRead(ArrayBottoni[PosizioneRandom]) == HIGH)
+        {
+          Punti_PartitaCorrente++;
+          finito = true;
+        }
+         Tempo2=millis();
+         if (Tempo2- Tempo1 >=2000)
+         {
+          finito = true;
+          Vite--;
+         }
+      }
+    }
+    else if(NumeroRandom >=6 && NumeroRandom <= 7)
+    {
+      //malus
+    }
+    else
+    {
+      //bonus
+    }
+    
+  }
+  
+  
+  
+  
+  
+  
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void loop() {
+  AggiornaRecord( Record, Punti_PartitaCorrente);
+  Vite = 3;
+  Punti_PartitaCorrente = 0;
+  SchermataIniziale();
+  BottoneInizioMetodo(BottoneIniziale);
+  Gioco();
+ 
   
 
 }
